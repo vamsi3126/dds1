b8e15c21cd38b3db61282a05969c9e4bdc8ea1e7
vamsi3126 <123225757+vamsi3126@users.noreply.github.com>
2025-11-08 00:33:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def start_detection():
     return "Drowsiness Detection Started! Close the camera window to stop."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
